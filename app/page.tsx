6bf1f0a727ec4b3d4e2a1e101226010d9b81fe92
pyytryy <putripitamutia99@gmail.com>
2026-07-11 23:21:15 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -615,7 +615,6 @@ export default function Home() {
             <div className="mt-8 space-y-4 text-xl text-gray-700">
               <p>📍 Hotel Puri Senyiur</p>
               <p>⏰ Minggu, 21 Juni 2026. 08.30 WITA</p>
-              <p> (kalo sebisa sebelum itu ya gais ngumpulnyaa, biar bisa foto ramean lengkap semua yaww)</p>
             </div>
           </div>
 
